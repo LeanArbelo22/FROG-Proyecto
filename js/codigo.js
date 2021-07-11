60ff05aa9ac2af054c5f8cf174706ad9d92cbdda
Leandro Arbelo <leo.arbelo22@gmail.com>
2021-07-11 12:25:06 -0300

    SOLUCIONADO FUNCION MAPAS!!

--- js/codigo.js
+++ js/codigo.js
@@ -1,13 +1,19 @@
-function mostrarMapaF8(){
-document.getElementById('mapita').innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3271.973223029184!2d-56.212886085041845!3d-34.907121481121!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959f7f83c152a7b7%3A0xdcb0ab61c78c7b8b!2sFrog%208!5e0!3m2!1ses-419!2suy!4v1625419246973!5m2!1ses-419!2suy" width="1100" height="220" style="border:5px solid rgba(34, 34, 34, 0.877);" allowfullscreen="" loading="lazy"></iframe>` 
-}
-
-function mostrarMapaF13(){
-    document.getElementById('mapita').innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3271.992107237552!2d-56.19127198504188!3d-34.90664758109577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959f81cce4363b73%3A0x7b73d4ef34b3cab8!2sFrog%2013!5e0!3m2!1ses-419!2suy!4v1625419297724!5m2!1ses-419!2suy" width="1100" height="220" style="border:5px solid rgba(34, 34, 34, 0.877);" allowfullscreen="" loading="lazy"></iframe>`
-}
-function mostrarMapaF15(){
-    document.getElementById('mapita').innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3271.993446138952!2d-56.18002648504178!3d-34.90661398109399!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959f81b68248a507%3A0x5462a2bff21faf54!2sFrog%2015!5e0!3m2!1ses-419!2suy!4v1625419180616!5m2!1ses-419!2suy" width="1100" height="220" style="border:5px solid rgba(34, 34, 34, 0.877);" allowfullscreen="" loading="lazy"></iframe>`
-}
-function mostrarMapaF22(){
-    document.getElementById('mapita').innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3272.582963446775!2d-56.07355088504238!3d-34.89181718030503!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959f8709ffe0da1d%3A0x210cd8b63d7cf803!2sFrog%20maxishop%2022!5e0!3m2!1ses-419!2suy!4v1625419393781!5m2!1ses-419!2suy" width="1100" height="220" style="border:5px solid rgba(34, 34, 34, 0.877);" allowfullscreen="" loading="lazy"></iframe>`
+function mapas(frog) {
+    let span = document.getElementById('mapita');
+    
+    switch (frog){
+        case 8:
+            span.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3271.973223029184!2d-56.212886085041845!3d-34.907121481121!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959f7f83c152a7b7%3A0xdcb0ab61c78c7b8b!2sFrog%208!5e0!3m2!1ses-419!2suy!4v1625419246973!5m2!1ses-419!2suy" width="1100" height="220" style="border:5px solid rgba(34, 34, 34, 0.877);" allowfullscreen="" loading="lazy"></iframe>`;
+            break;
+        case 13: 
+            span.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3271.992107237552!2d-56.19127198504188!3d-34.90664758109577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959f81cce4363b73%3A0x7b73d4ef34b3cab8!2sFrog%2013!5e0!3m2!1ses-419!2suy!4v1625419297724!5m2!1ses-419!2suy" width="1100" height="220" style="border:5px solid rgba(34, 34, 34, 0.877);" allowfullscreen="" loading="lazy"></iframe>`;
+            break;
+        case 15:
+            span.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3271.993446138952!2d-56.18002648504178!3d-34.90661398109399!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959f81b68248a507%3A0x5462a2bff21faf54!2sFrog%2015!5e0!3m2!1ses-419!2suy!4v1625419180616!5m2!1ses-419!2suy" width="1100" height="220" style="border:5px solid rgba(34, 34, 34, 0.877);" allowfullscreen="" loading="lazy"></iframe>`;
+            break;
+        case 22:
+            span.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3272.582963446775!2d-56.07355088504238!3d-34.89181718030503!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959f8709ffe0da1d%3A0x210cd8b63d7cf803!2sFrog%20maxishop%2022!5e0!3m2!1ses-419!2suy!4v1625419393781!5m2!1ses-419!2suy" width="1100" height="220" style="border:5px solid rgba(34, 34, 34, 0.877);" allowfullscreen="" loading="lazy"></iframe>`;
+            break;
+    }
+    
 }
